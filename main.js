62a24855f8f15ec291c8b9708d29dda07124dded
Dominik Santoro <174945216+dhz-77@users.noreply.github.com>
2024-08-15 16:24:59 +0200

    Simplify code
    Code is simplified, but now the grid colors are not being saved
    anymore. The color changes each time a grid item is mouse-overed.

--- main.js
+++ main.js
@@ -7,8 +7,7 @@ document.addEventListener("DOMContentLoaded", function() {
 
     let size = 16 // = width == height
 
-    setGrid(size);
-    setEventListener();
+    drawGrid(size);
 
     btn.addEventListener("click", () => {
         const gridSize = window.prompt("Enter a number up to 100:", size);
@@ -22,78 +21,33 @@ document.addEventListener("DOMContentLoaded", function() {
         } else {
             container.innerHTML = "";
             size = gridSize;
-            setGrid(size);
-            setEventListener();
+            drawGrid(size);
         }
     })
 
-    function setGrid(size) { // Create grid items and add to container div
+    function drawGrid(size) { // Create grid items and add to container div
         for (let i = 0; i < (size ** 2); i++) {
                 const gridItem = document.createElement("div");
                 gridItem.classList.add("grid-item");
-                gridItem.style.width = (960 / (size)) + "px"
+                gridItem.style.width = (700 / (size)) + "px"
                 container.appendChild(gridItem);
             }
+        setEventListener();
     }
     
     function setEventListener() {
         let elementsArray = document.querySelectorAll(".grid-item");
         
         elementsArray.forEach(function(elem) {
-            let count = 0;
-
+            let opc = 0;
             elem.addEventListener("mouseover", () => {
-                
-                count++;
-                console.log(count);
-
-                if (count > 10) {
-                    return;
-                }
-
-                if (count === 1) {
-                    elem.style.opacity = 0.1;
-                    elem.style.backgroundColor = randomRGB();
-                }
-                else if (count === 2) {
-                    elem.style.opacity = 0.2;
-                }
-                else if (count === 3) {
-                    elem.style.opacity = 0.3;
-                }
-                else if (count === 4) {
-                    elem.style.opacity = 0.4;
-                }
-                else if (count === 5) {
-                    elem.style.opacity = 0.5;
-                }
-                else if (count === 6) {
-                    elem.style.opacity = 0.6;
-                }
-                else if (count === 7) {
-                    elem.style.opacity = 0.7;
-                }
-                else if (count === 8) {
-                    elem.style.opacity = 0.8;
-                }
-                else if (count === 9) {
-                    elem.style.opacity = 0.9;
-                }
-                else if (count === 10) {
-                    elem.style.opacity = 1;
-                }
-
+                elem.style.backgroundColor = `rgb(${randColorNum()},${randColorNum()},${randColorNum()},${opc += 0.1})`;
             });
         });
-    }
 
-    function randColorNum() {
-        return Math.floor(Math.random() * 256);
-    }
-    
-    function randomRGB() {
-        
-        return `rgb(${randColorNum()},${randColorNum()},${randColorNum()})`;
+        function randColorNum() {
+            return Math.floor(Math.random() * 256);
+        }
     }
 
 });
