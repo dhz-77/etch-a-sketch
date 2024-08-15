4b31837b98056c69f41e90eebc61574038d0c4ed
Dominik Santoro <174945216+dhz-77@users.noreply.github.com>
2024-08-15 12:48:34 +0200

    Remove unused code

--- main.js
+++ main.js
@@ -27,37 +27,25 @@ document.addEventListener("DOMContentLoaded", function() {
         }
     })
 
-    // Loop to create grid items and append them to the container div
-    function setGrid(rows, columns) {
+    function setGrid(rows, columns) { // Create grid items and add to container div
         for (let i = 0; i < rows; i++) {
             for (let j = 0; j < columns; j++) {
                 const gridItem = document.createElement("div");
                 gridItem.classList.add("grid-item");
-    
-                // Set the position of each grid item
-                 //gridItem.style.left = j * 100 + "px";
-                 //gridItem.style.top = i * 100 + "px";
-                
                 gridItem.style.width = (960 / (rows)) + "px"
                 container.appendChild(gridItem);
             }
         }
-
     }
     
     function setEventListener() {
         let elementsArray = document.querySelectorAll(".grid-item");
 
         elementsArray.forEach(function(elem) {
-        elem.addEventListener("mouseover", () => {
-            elem.style.backgroundColor = "blue";
-        });
-
-        /* optional hover effect - does not leave trail
-        elem.addEventListener("mouseout", () => {
-            elem.style.backgroundColor = "";
-        }); */
+            elem.addEventListener("mouseover", () => {
+                elem.style.backgroundColor = "blue";
+            });
         });
     }
-    
+
 });
