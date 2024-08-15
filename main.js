fd794e27c469fedd1469537c6bdc4acf546f15e4
Dominik Santoro <174945216+dhz-77@users.noreply.github.com>
2024-08-15 10:33:26 +0200

    Add button to resize grid
    When the button is clicked and a number is entered, the old grid is
    removed and a new grid with a size equal to the number created.

--- main.js
+++ main.js
@@ -3,29 +3,40 @@
 document.addEventListener("DOMContentLoaded", function() {
     
     const container = document.querySelector("#container");
+    const btn = document.querySelector("button");
 
-    // Create a 16x16 grid
-    const rows = 16;
-    const columns = 16;
+    setGrid(16, 16);
+    setEventListener();
+
+    btn.addEventListener("click", () => {
+        const gridSize = Number(window.prompt("Enter a number up to 100:", "16"));
+
+        container.innerHTML = "";
+        let rows = gridSize, columns = gridSize;
+        setGrid(rows, columns);
+        setEventListener();
+    })
 
     // Loop to create grid items and append them to the container div
-    for (let i = 0; i < rows; i++) {
-        for (let j = 0; j < columns; j++) {
-            const gridItem = document.createElement("div");
-            gridItem.classList.add("grid-item");
-
-            // Set the position of each grid item
-            gridItem.style.left = j * 100 + "px";
-            gridItem.style.top = i * 100 + "px";
-
-            container.appendChild(gridItem);
-        }
+    function setGrid(rows, columns) {
+        for (let i = 0; i < rows; i++) {
+            for (let j = 0; j < columns; j++) {
+                const gridItem = document.createElement("div");
+                gridItem.classList.add("grid-item");
+    
+                // Set the position of each grid item
+                gridItem.style.left = j * 100 + "px";
+                gridItem.style.top = i * 100 + "px";
+    
+                container.appendChild(gridItem);
+            }
+        }    
     }
+    
+    function setEventListener() {
+        let elementsArray = document.querySelectorAll(".grid-item");
 
-
-    let elementsArray = document.querySelectorAll(".grid-item");
-
-    elementsArray.forEach(function(elem) {
+        elementsArray.forEach(function(elem) {
         elem.addEventListener("mouseover", () => {
             elem.style.backgroundColor = "blue";
         });
@@ -34,6 +45,7 @@ document.addEventListener("DOMContentLoaded", function() {
         elem.addEventListener("mouseout", () => {
             elem.style.backgroundColor = "";
         }); */
-    });
+        });
+    }
     
 });
